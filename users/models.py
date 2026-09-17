1125a1a3f686aa2e63029094d5a1adc21ce5dfc9
djumanov <djumanovdev@gmail.com>
2026-09-17 21:16:09 +0500

    account model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Account(models.Model):
+    username   = models.CharField(
+        null=False,
+        blank=False,
+        unique=True,
+        max_length=64,
+        validators=[MinLengthValidator(3)]
+    )
+    email      = models.EmailField(max_length=256)
+    password   = models.CharField(max_length=256)
+    first_name = models.CharField(
+        max_length=64,
+        validators=[MinLengthValidator(3)]
+    )
+    last_name  = models.CharField(
+        max_length=64,
+        validators=[MinLengthValidator(3)]
+    )
+    bio        = models.TextField(default='')
+    birth_date = models.DateField()
+    is_active  = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
