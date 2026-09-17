ef29ab29f4cea0df95bd271335dabc6e1ad65e03
djumanov <djumanovdev@gmail.com>
2026-09-17 21:29:35 +0500

    task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Account
+
+
+class Task(models.Model):
+    name        = models.CharField(max_length=127)
+    description = models.TextField()
+    completed   = models.BooleanField(default=False, blank=True)
+    created_at  = models.DateTimeField(auto_now=True)
+    updated_at  = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
