c95875521f9e10b4863e33f3677e3b1be7c3165e
djumanov <djumanovdev@gmail.com>
2026-09-19 15:56:35 +0500

    task model

--- tasks/models.py
+++ tasks/models.py
@@ -4,10 +4,20 @@ from users.models import Account
 
 
 class Task(models.Model):
-    name        = models.CharField(max_length=127)
-    description = models.TextField()
+    class PriorityChoices(models.TextChoices):
+        low    = 'low', 'Past'
+        middle = 'middle', 'O\'rta'
+        high   = 'high', 'Yuqori'
+
+    title        = models.CharField(max_length=127)
+    description = models.TextField(help_text='taskni tarifini kiriting...')
     completed   = models.BooleanField(default=False, blank=True)
+    priority    = models.CharField(max_length=20, blank=True, choices=PriorityChoices, default=PriorityChoices.middle, verbose_name='muhumlik darajasi')
     created_at  = models.DateTimeField(auto_now=True)
     updated_at  = models.DateTimeField(auto_now_add=True)
 
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='tasks')
+
+
+class Item(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
