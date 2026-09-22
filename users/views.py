b5227c9756a3801cab502537a489ca06cb0c5238
djumanov <djumanovdev@gmail.com>
2026-09-22 20:13:29 +0500

    register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
 
-# Create your views here.
+from django.http import HttpRequest, JsonResponse
+
+from .models import Account
+
+
+def register_view(request: HttpRequest) -> JsonResponse:
+    if request.method != 'POST':
+        return JsonResponse(
+            data={
+                'status': 'error',
+                'message': 'method not allowed.'
+            },
+            status=404
+        )
+
+    data = json.loads(request.body)
+
+    try:
+        Account.objects.get(username=data['username'])
+        return JsonResponse(
+            data={
+                'status': 'error',
+                'message': 'account already exists.'
+            },
+            status=400
+        )
+    except Account.DoesNotExist:
+        account = Account(
+            username=data['username'],
+            email=data['email'],
+            password=data['password'],
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            bio=data.get('bio'),
+            birth_date=datetime.strptime(
+                data['birth_date'],
+                '%d.%m.%Y'
+            ),
+        )
+        account.save()
+
+        return JsonResponse(
+            data={
+                'status': 'success',
+                'message': 'account has been created.'
+            },
+            status=201
+        )
